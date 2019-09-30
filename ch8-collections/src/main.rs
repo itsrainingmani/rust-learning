0117ac2b1b76f57919c90ab51c3d6542446474b6
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-09-29 23:38:53 -0400

    Work with Vectors. Starting with Strings

--- ch8-collections/src/main.rs
+++ ch8-collections/src/main.rs
@@ -19,4 +19,56 @@ fn main() {
     v.push(5);
     v.push(6);
     println!("{:#?}", v);
+
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
+
+    match v.get(2) {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no third element"),
+    }
+
+    // let does_not_exist = &v[100];
+    let does_not_exist = v.get(100);
+
+    // let first = &v[0];
+
+    // v.push(0);
+
+    // println!("The first element is {}", first);
+
+    let v1 = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50; // De-referencing the ref to an element and incrementing
+    }
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row: Vec<SpreadsheetCell> = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Float(10.12),
+        SpreadsheetCell::Text(String::from("blue")),
+    ];
+
+    let mut s = String::new();
+
+    let data = "initial contents";
+
+    let s1 = data.to_string();
+    let s2 = "initial contents".to_string();
+
+    let s3 = String::from(data);
+
+    // UTF-8 encoded Strings
+    let hello = String::from("السلام عليكم");
+
+    s.push_str(&hello);
+    s.push_str(" ");
+    s.push_str(data);
+
+    println!("{}", s);
 }
