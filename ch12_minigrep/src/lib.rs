b4b1b0dd7d928880730aa957b69ed82147ab9b3a
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-10-03 16:11:06 -0400

    Move struct, impl and run to lib.rs

---
+++ ch12_minigrep/src/lib.rs
@@ -0,0 +1,31 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
+}
+
+// Box<dyn Error> is a trait object - The function returns a type
+// that will implement the Error trait
+// dyn is short for dynamic
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(()) // Success type is the unit type ().
+           //Indicates that we are calling run only for it's side effects
+}
