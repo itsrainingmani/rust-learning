bd7be0c15b5d0cbd0058b823f2d3dca50d3d5b48
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-10-04 11:54:37 -0400

    Add search function that checks for lines containing the term and returns a vector of matching lines

--- ch12_minigrep/src/lib.rs
+++ ch12_minigrep/src/lib.rs
@@ -63,7 +63,14 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
 }
 
 // Box<dyn Error> is a trait object - The function returns a type
@@ -72,7 +79,9 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(()) // Success type is the unit type ().
            //Indicates that we are calling run only for it's side effects
