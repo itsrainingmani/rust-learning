15606ba4a0aeeacdb6168d2489dec8f59eb9bc86
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-10-04 12:06:58 -0400

    Add test and function for case insensitive search and use it in the body of the run method depending on value of the env var

--- ch12_minigrep/src/lib.rs
+++ ch12_minigrep/src/lib.rs
@@ -34,20 +34,34 @@ mod tests {
     // }
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust;
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust;
+safe, fast, productive.
+Pick three.
+Trust me";
+
+        assert_eq!(vec!["Rust;", "Trust me"], search_case_insensitive(query, contents));
     }
 }
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -73,13 +87,31 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
 // Box<dyn Error> is a trait object - The function returns a type
 // that will implement the Error trait
 // dyn is short for dynamic
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{}", line);
     }
 
