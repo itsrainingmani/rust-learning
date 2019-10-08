3230c8b762e98f6e218ca56f6fff8961960f832e
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-10-08 11:58:02 -0400

    Implement HashMap for the Cacher

--- ch13_functional/src/main.rs
+++ ch13_functional/src/main.rs
@@ -1,25 +1,34 @@
+use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
 
-struct Cacher<T> where T: Fn(u32) -> u32 {
+struct Cacher<T, K, V>
+where
+    T: Fn(K) -> V,
+{
     calculation: T,
-    value: Option<u32>
+    values: HashMap<K, V>,
 }
 
-impl<T> Cacher<T> where T: Fn(u32) -> u32 {
-    fn new(calculation: T) -> Cacher<T> {
+impl<T, K, V> Cacher<T, K, V>
+where
+    T: Fn(K) -> V,
+    K: std::cmp::Eq + std::hash::Hash + Copy,
+    V: Copy,
+{
+    fn new(calculation: T) -> Cacher<T, K, V> {
         Cacher {
             calculation,
-            value: None,
+            values: HashMap::new(),
         }
     }
 
-    fn value(&mut self, arg: u32) -> u32 {
-        match self.value {
-            Some(v) => v,
+    fn value(&mut self, arg: K) -> V {
+        match self.values.get(&arg) {
+            Some(v) => *v,
             None => {
                 let v = (self.calculation)(arg);
-                self.value = Some(v);
+                self.values.insert(arg, v);
                 v
             }
         }
@@ -29,35 +38,30 @@ impl<T> Cacher<T> where T: Fn(u32) -> u32 {
 fn main() {
     println!("Chapter 13: Functional Concepts");
 
-    let simulated_user_specified_value = 10;
+    let simulated_user_specified_value = 30;
     let simulated_random_number = 7;
 
     generate_workout(simulated_user_specified_value, simulated_random_number);
-
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_result = Cacher::new(|num| {
+    let mut expensive_result = Cacher::new(|num| {
         println!("Calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
     });
 
     if intensity < 25 {
-        println!(
-            "Today do {} pushups", 
-            expensive_result.value(intensity)
-        );
-
-        println!(
-            "Next do {} situps", 
-            expensive_result.value(intensity)
-        );
+        println!("Today do {} pushups", expensive_result.value(intensity));
+        println!("Next do {} situps", expensive_result.value(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated");
         } else {
-            println!("Today run for {} minutes", expensive_result(intensity));
+            println!(
+                "Today run for {} minutes",
+                expensive_result.value(intensity)
+            );
         }
     }
 }
