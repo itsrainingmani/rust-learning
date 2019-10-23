8340c8be37d6a50c1481527225f66a5535cbd1fd
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-10-23 16:11:24 -0400

    Basic pattern matching examples

---
+++ ch18_patterns/src/main.rs
@@ -0,0 +1,45 @@
+fn main() {
+    println!("Chapter 18. Patterns");
+
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "23".parse();
+
+    if let Some(color) = favorite_color {
+        println!("Using your fave color {} as the background", color);
+    } else if is_tuesday {
+        println!("Tuesday is Green Day");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        }
+    } else {
+        println!("Using blue as the background color");
+    }
+
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
+
+    let v = vec!['a', 'b', 'c'];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
+    }
+
+    // let (x, y, _) = (1, 2, 3);
+    fn print_coordinates(&(x, y): &(i32, i32)) {
+        println!("Current location: ({}, {})", x, y);
+    }
+
+    let point = (3, 5);
+    print_coordinates(&point);
+}
